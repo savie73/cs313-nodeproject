b20e0089f961991f3f930436d035d0193339b3a1
Savannah Pyle <bea12022@byui.edu>
2018-07-11 20:10:47 -0600

    remove files

--- public/client.js
+++ public/client.js
@@ -1,37 +1,33 @@
 function getResults2(request, response) {
-	var source =  $("#price").val(); //do I need to use this in the update section?
-	var skin_id = $("#skin").val();
+    var source =  $("#price").val();
+    var skin_id = $("#skin").val();
+    
+    console.log("Searching for data..");    
 
-	// $('input[name=name_of_your_radiobutton]:checked').val();
-	
-	console.log("Searching for data..");
+    $.get("/getResults?price=" + source + "&skin=" + skin_id, function(data, status) {
 
-	
+        console.log("Back from server with the following results:")
+        console.log(status);
+        console.log(data);
 
-	$.get("/getResults?price=" + source + "&skin=" + skin_id, function(data, status) {
+        var resultsD = $("#results");
 
-		console.log("Back from server with the following results:")
-		console.log(status);
-		console.log(data);
-
-		var resultsD = $("#results");
-
-		for (let row of data)
-			{
-				resultsD.append(
-      			"<div class=\"w3-col l3 s6\">" +
-      			"<div class=\"w3-container\">" +
-				"<img src=\"/images/\"" + row['image'] + " alt=\"alt text\"  width=\"100%\" />" +
-      			' ' + row['brand'] +
-      			' ' + row['product_name'] +
-      			' $' + row['price'] +
-		  		"<br/>" +
-      			"</div>" +
-      			"</div>"
-      			);
-			}
-	
-	});
+        for (let row of data)
+            {
+                resultsD.append(
+                '<div class="w3-col l3 s6">' +
+                '<div class="w3-container">' +
+                '<img src="' + row['image'] + '" alt="alt text"  width="100%" />' +
+                ' ' + row['brand'] +
+                ' ' + row['product_name'] +
+                ' $' + row['price'] +
+                "<br/>" +
+                "</div>" +
+                "</div>"
+                );
+            }
+    
+    });
 
 }
 
